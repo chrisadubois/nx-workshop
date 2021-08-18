890bdd6efcf75156b74c9037cac0cf0ed67ce715
Christopher DuBois <chdubois@cisco.com>
2021-08-18 11:42:48 -0700

    feat(lab14): scopes

---
+++ bg-hoard/tools/generators/update-scope-schema/index.ts
@@ -0,0 +1,47 @@
+import { Tree, formatFiles, installPackagesTask, updateJson, readJson } from '@nrwl/devkit';
+
+function getScopes(nxJson: any) {
+  const projects: any[] = Object.values(nxJson.projects);
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags
+        // take only those that point to scope
+        .filter((tag: string) => tag.startsWith('scope:'))
+    )
+    // flatten the array
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    // remove prefix `scope:`
+    .map((scope: string) => scope.slice(6));
+  // remove duplicates
+  return Array.from(new Set(allScopes));
+}
+
+function replaceScopes(content: string, scopes: string[]): string {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
+  return content.replace(
+    PATTERN,
+    `interface Schema {
+  name: string;
+  directory: ${joinScopes};
+}`
+  );
+}
+
+export default async function (tree: Tree, schema: any) {
+  const scopes = getScopes(readJson(tree, 'nx.json'));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', (schemaJson) => {
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
+
+    schemaJson.properties.directory.enum = scopes;
+    return schemaJson;
+  });
+
+  const content = tree.read('tools/generators/util-lib/index.ts', 'utf-8');
+  const newContent = replaceScopes(content, scopes);
+  tree.write('tools/generators/util-lib/index.ts', newContent);
+  await formatFiles(tree);
+}
